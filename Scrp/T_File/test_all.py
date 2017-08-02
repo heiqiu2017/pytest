5437c1c2e0280d961aaf68ce3e8abe6bf67484fa
lizhaolin <zhaolin.li@finlink.net.cn>
2017-08-02 15:28:50 +0800

    Initial commit

---
+++ Scrp/T_File/test_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import allure
+class Test_Sample:
+    def test_login(self):
+        allure.attach('描述', '这是一个注册登录的case')
+        self.register()
+        result = self.login('mio4kon')
+        assert result
+    @allure.step(title="登录账号:{1}")
+    def login(self, account):
+        return True
+    @allure.step(title="注册")
+    def register(self):
+        pass
+    def test_foo(self):
+        f = open('./Image/1498470308.png', 'rb').read()
+        # allure.attach('my attach', 'Hello, World')
+
+        assert 0,allure.attach('this is a img', f, allure.attach_type.PNG)
